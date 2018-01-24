9bc67f70062f63c6529dbda194492142fbb7bf49
Tom <thomas.verin@sonarsource.com>
2018-01-24 09:22:02 +0100

    Fix timezone dateformat

--- vars/formatTimestamp.groovy
+++ vars/formatTimestamp.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
 def String call(timestamp) {
-  java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+  java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ");
   return sdf.format(timestamp)
 }
