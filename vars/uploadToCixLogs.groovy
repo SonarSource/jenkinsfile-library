4dfa99257a6a6199a24ab0a1ef1fcb5bc2e78a02
David Rautureau <david.rautureau@sonarsource.com>
2018-01-24 10:35:54 +0100

    Add helper to upload logs to cix-wlogs in case of failure (#15)

---
+++ vars/uploadToCixLogs.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/groovy
+
+def call(def stage) {
+  def remoteDir = "${env.CIX_WLOGS_HOME}".plus("/${env.BUILD_TAG},${stage}".replaceAll('/', ','))
+  sh "ssh -o \"StrictHostKeyChecking no\" ${env.CIX_WLOGS_HOST} \"mkdir -p ${remoteDir}\""
+  sh "rsync -ra -e ssh --include '*/' --include='*.log' --include='*.txt' --include='*.conf' --include='*.xml' --include='*.properties' --exclude='*' --prune-empty-dirs * ${env.CIX_WLOGS_HOST}:${remoteDir}"
+  sh "ssh -o \"StrictHostKeyChecking no\" ${env.CIX_WLOGS_HOST} \"zip -qr ${remoteDir}.zip ${remoteDir}\""
+}
