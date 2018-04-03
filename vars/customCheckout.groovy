31a9d56f83be8e70723a4d7e684c70547c88eab4
tomverin <thomas.verin@sonarsource.com>
2018-04-03 16:12:23 +0200

    Add custom checkout with PRmerge functionnality (#19)

---
+++ vars/customCheckout.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/groovy
+
+def call() { 
+  checkout scm   
+  if (GITHUB_BRANCH.startsWith('PULLREQUEST-')){    
+    def prNumber = GITHUB_BRANCH.minus('PULLREQUEST-')
+    try{      
+      sh "git branch -D prmerge$prNumber"
+      echo "removed already existing prmerge$prNumber branch, ready to fetch last merge commit"
+    } catch(e) {
+      echo "ready to checkout PR $prNumber merge commit"
+    }
+    //fetch the PR merge commit
+    sh "git pull origin pull/$prNumber/merge:prmerge$prNumber"
+    //checkout the PR merge commit
+    sh "git checkout prmerge$prNumber"
+  }
+}
