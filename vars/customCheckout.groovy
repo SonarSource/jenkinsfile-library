087abc33de62d1ba14dd4cb52eec911a406bccb8
tomverin <thomas.verin@sonarsource.com>
2018-04-09 09:48:15 +0200

    Removes master checkout (#23)

--- vars/customCheckout.groovy
+++ vars/customCheckout.groovy
@@ -12,8 +12,6 @@ def call() {
         echo "ready to checkout PR $prNumber merge commit"
       }
     }
-    //checkout master
-    sh "git checkout $GITHUB_TARGET_BRANCH"
     //fetch the PR merge commit
     sh "git pull origin pull/$prNumber/merge:prmerge$prNumber"
     //checkout the PR merge commit
