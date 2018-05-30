f7c79ba4a7d34ee19dc558b1827a294807a69b8b
tomverin <thomas.verin@sonarsource.com>
2018-05-30 09:23:11 +0200

    Add png screenshot upload to cix-wlogs (#25)

--- vars/uploadToCixLogs.groovy
+++ vars/uploadToCixLogs.groovy
@@ -4,7 +4,7 @@ def call(def stage) {
   def shortDir = "/${env.BUILD_TAG},${stage}".replaceAll('/', ',')
   def remoteDir = "${env.CIX_WLOGS_HOME}".plus(shortDir)
   sh "ssh -o \"StrictHostKeyChecking no\" ${env.CIX_WLOGS_HOST} \"mkdir -p ${remoteDir}\""
-  sh "rsync -ra -e ssh --include '*/' --include='*.log' --include='*.txt' --include='*.conf' --include='*.xml' --include='*.properties' --include='*.html' --exclude='*' --prune-empty-dirs * ${env.CIX_WLOGS_HOST}:${remoteDir}"
+  sh "rsync -ra -e ssh --include '*/' --include='*.log' --include='*.txt' --include='*.conf' --include='*.xml' --include='*.properties' --include='*.html' --include='*.png' --exclude='*' --prune-empty-dirs * ${env.CIX_WLOGS_HOST}:${remoteDir}"
   sh "ssh -o \"StrictHostKeyChecking no\" ${env.CIX_WLOGS_HOST} \"zip -qr ${remoteDir}.zip ${remoteDir}\""
   echo "TO INVESTIGATE TEST FAILURE: Go to https://cix-wlogs.sonarsource.com/$shortDir"
 }
